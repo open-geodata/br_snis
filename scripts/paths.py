dce1f5aa65585d848a3c000522b5cddf654b53d1
Michel Metran <michelmetran@gmail.com>
2023-02-22 20:15:22 -0300

    Ajustes

--- scripts/paths.py
+++ scripts/paths.py
@@ -8,19 +8,28 @@ from pathlib import Path
 project_path = Path(__file__).parents[1]
 
 
-docs_path = project_path / 'docs'
-docs_path.mkdir(exist_ok=True)
+# Datasets Path
+dataset_path = Path(project_path.anchor) / 'Datasets' / project_path.name
+dataset_path.mkdir(exist_ok=True)
 
-# Pastas
-data_path = project_path / 'data'
+# Data
+data_path = dataset_path / 'data'
 data_path.mkdir(exist_ok=True)
 
+# Input
 input_path = data_path / 'input'
 input_path.mkdir(exist_ok=True)
 
+input_path_temp = input_path / 'temp'
+input_path_temp.mkdir(exist_ok=True)
+
+# Output
 output_path = data_path / 'output'
 output_path.mkdir(exist_ok=True)
 
+# Docs
+docs_path = project_path / 'docs'
+docs_path.mkdir(exist_ok=True)
 
 if __name__ == '__main__':
     print(project_path)
